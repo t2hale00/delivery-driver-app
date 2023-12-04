4a5be9618cb1982ede79bfc7d7ba871de40bd221
“ShujuanXu” <t2xush00@students.oamk.fi>
2023-12-04 15:21:30 +0200

    new version,select locker,get freecabinet

--- backend/server.js
+++ backend/server.js
@@ -16,109 +16,27 @@ const db = mysql.createConnection({
   database: "driverdb",
 });
 
-app.get("/getCabinets", (req, res) => {
-  const { locker, status } = req.query;
+app.get("/getFreeCabinets",(req,res)=>{
+    const {locker}=req.query;
 
-  let query = "select * from cabinets where location=?";
-  if (status === "available") {
-    query = "select * from cabinets where location=? and cabinetstatus = 'available'";
-  } else {
-    query = " select * from cabinets where location=? and cabinetstatus='todelivery'";
-  }
-
-  db.query(query, [locker], (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-      console.log("got data");
-    }
-  });
-});
-
-app.get("/getParcels", (req, res) => {
-  const { locker } = req.query;
-
-  db.query(
-    "select * from parcels where status='undelivered'",
-   
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send(result);
-        console.log("got undelivery parcels");
-      }
-    }
-  );
-});
-
-app.put("/update",(req,res)=>{
-  const cabinetid=req.body.cabinetid;
-  const parcelid=req.body.parcelid;
-  db.query(
-    "update cabinets as c "+" join parcels as p "+" on c.code=p.reservationcode "+" set c.cabinetstatus =case when c.cabinetstatus='available' then 'topickup' when c.cabinetstatus='todelivery' then 'available' else c.cabinetstatus end , " +"  p.status=case when p.status='todelivery' then 'undelivered' else p.status end, p.iscodevalid=true where c.cabinetid=?",[cabinetid],
-    (err,result)=>{
-      if(err){
-        console.log(err);
-      }else {
-        res.send(`cabinet${cabinetid}status changed,${parcelid}status changed`);
-        console.log(`${cabinetid}status changed,${parcelid}status changed`)
-      }
-    }
-  )
-
+    db.query("select * from cabinets where cabinetstatus='available' and location =?",[locker],(err,result)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.send(result);
+            console.log(result);
+        }
+    })
 })
 
 
-// app.put("/update", (req, res) => {
-//   const id = req.body.id;
-//   const cabinetstatus = req.body.cabinetstatus;
-//   const parcelid = req.body.parcelid;
-
-//   if (cabinetstatus === "todelivery") {
-//     db.query(
-//       "update cabinets set cabinetstatus='available' where id=?",
-//       [id],
-//       (err, result) => {
-//         if (err) {
-//           console.log(err);
-//         } else {
-//           res.send(`${id}changed status from 'occupied' to 'available`);
-//           console.log(`cabinet${id}changed status from 'occupied' to 'available`);
-//         }
-//       }
-//     );
-//   } else if (cabinetstatus === "available") {
-//     const sqlQuery1 = "UPDATE cabinets SET status='waittopickup', iscodevalid=true WHERE id=?";
-// const sqlQuery2 = "UPDATE parcel SET status='waittopickup' WHERE parcelid=?";
-
-// db.query(sqlQuery1, [id], (err1, result1) => {
-//   if (err1) {
-//     console.log(err1);
-//   } else {
-//     // Execute the second query
-//     db.query(sqlQuery2, [parcelid], (err2, result2) => {
-//       if (err2) {
-//         console.log(err2);
-//       } else {
-//         res.send(`${id} changed status from 'available' to 'occupied', parcel status changed to 'waittopickup`);
-//         console.log(`${id} changed status from 'available' to 'occupied`);
-//       }
-//     });
-//   }
-// });
-
-
-    
-//   }
-// });
 
 
 app.listen(3003, () => {
-  console.log("server for driver is running on port3003");
-  db.connect(function (err) {
-    if (err) throw err;
-    console.log("database connected");
+    console.log("server for driver is running on port3003");
+    db.connect(function (err) {
+      if (err) throw err;
+      console.log("database connected");
+    });
   });
-});
+  
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,70 @@
-
-import './App.css';
-import MainPanel from './components/MainPanel';
-
-
+import "./App.css";
+import React, { useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [selectedLocker, setSelectedLocker] = useState("");
+  const [message, setMessage] = useState("");
+  const [freeCabinets, setFreeCabinets] = useState([]);
+  const [showfreecabinets,setShowFreeCabinets]=useState(false)
+
+  const handleLockerSelect = (event) => {
+    const lockerValue = event.target.value;
+    setSelectedLocker(lockerValue);
+    setMessage("");
+    Axios.get("http://localhost:3003/getFreeCabinets", {
+      params: { locker: lockerValue },
+    })
+      .then((response) => {
+        setFreeCabinets(response.data);
+      })
+      .catch((err) => {
+        console.log("erro fetching ");
+      });
+  };
+
+
+
+
   return (
     <div className="App">
-           <MainPanel/>
-      
+      {/* for lockerselect */}
+      <div className="LockerSelect">
+        <lable className="LockerSelect" htmlFor="dropdown">
+          Select Locker:
+        </lable>
+        <select
+          className="LockerSelect"
+          id="dropwown"
+          value={selectedLocker}
+          onChange={handleLockerSelect}
+        >
+          <option value="">--Select--</option>
+          <option value="Locker1">Locker1</option>
+          <option value="Locker2">Locker2</option>
+          <option value="Locker3">Locker3</option>
+          <option value="Locker4">Locker4</option>
+          <option value="Locker5">Locker5</option>
+        </select>
+        {selectedLocker && (
+          <p className="LockerSelect">You selected:{selectedLocker}</p>
+        )}
+      </div>
+
+      <div>
+        {selectedLocker && (
+          <button className="showfreecabinet"
+          onClick={()=>{setShowFreeCabinets(!showfreecabinets)
+
+          }}>show/hide free cabinet</button>
+        )}
+        {showfreecabinets&&<div>{freeCabinets.map((freecabinet) => (
+          <div key={freecabinet.cabinetid} className="Box">
+            <p>number:{freecabinet.number}</p>
+            <p>status:{freecabinet.cabinetstatus}</p>
+          </div>
+        ))}</div>}
+      </div>
     </div>
   );
 }
